cc115f586ccb5e54150bca8e80561ebbf3b3c0ce
Wesley Zhang <wes4zhang@Wesleys-MBP.attlocal.net>
2019-01-18 22:50:36 -0800

    foo

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
 # shengji-bot
-### README IN PROGRESS
+### Made for the purpose of playing bā shí fēn shēng jí (80-point upgrade/tractor) by `[REDACTED]`. *Bot is buggy and may not reflect the actual game of shēngjí.*
+
+## Command list
+### Anytime
+`$intro` - Probably how you got here. Sends a link directing you to this file.
+
+### Before game starts
+`$join` - Join a game as the first available wind. Winds in order are North, West, South, East. The order determines teams (North with South and East with West.)
+
+`$leave` - Has the opposite effect of join - leave the game. Any people occupying winds behind you are moved up by one, an implementation that will probably be fixed soon.
+
+`$list` - Lists all players in the current game (and associated wind). 
+
+`$start` - Starts the game! 
+
+### During game
+`$deal` - Starts the dealing process. A card will be dealt to a person every two seconds.
+
+`$call <suit> [strong]` - Can be used at any time during the dealing process. `suit` must be a suit shorthand (see Appendix A). `strong`, if included and anything besides 0, will perform a strong call (see Appendix B). Otherwise, it's a weak call. *Each call type can only be done once per deal cycle, and weak calls cannot be done after a strong call.*
+
+`$kitty <*cards>` - Sets the kitty (see Appendix B). `cards` must contain eight arguments, each one an acceptable card in your hand (see Appendix A).
+
+`$play <*cards>` - Plays the cards specified. `cards` can contain any number of elements.
+
+`$score` - Shows the scoreboard (see Appendix B).
+
+`$surrender` - A way to say you're a bad player.
+
+`$reset` - Casts a vote to reset the game. *Resetting requires the approval of three people currently in the game.*
+
+## Appendix A: Formatting
+
+Cards are expressed as two alphanumeric characters (with the exception of tens), in order the rank and the suit (with the exception of Jokers).
+
+Examples:
+`2C` = Two of Clubs
+`4D` = Four of Diamonds
+`8H` = Eight of Hearts
+`10C` = Ten of Clubs
+`QS` = Queen of Spades
+`AD` = <abbr title="Star of India">Ace of Diamonds</abbr>
+`J-` = Black Joker
+`J+` = Red Joker
+ 
+ Suits are simply the last letter of each (again, excepting Jokers). 
+
+## Appendix B: How to play tractor/shēng jí
+
+**Shēng jí** (lit. upgrade, also known as **tuō lā jī**/**tractor**) is a trick-taking game (much like bridge). Any number of decks can be used in this game, but this bot handles the two-deck variant.
+
+The game requires four players, sitting in a circle. Two people sitting opposite each other are on a *team*.
+
+At game start, each team is at a *score* of 2. Teams can increase this score by winning rounds.
+
+### Round structure
+
+The dominant rank for each round is the score of the team of the current dealer. This dealer is determined by the results of previous rounds as follows:
+
+* In the first round, whoever declares a trump first immediately becomes the dealer, with his team becoming declarers.
+* In subsequent rounds, if the declarers won the previous round, the previous dealer's partner becomes dealer; otherwise, the other team becomes the declarers, and the person to the previous dealer's right becomes the new dealer. 
+
+Cards are not dealt in this game, rather drawn - the bot attempts to simulate this by dealing one card every half-second. At any time during the drawing process, a player can declare a suit to be trump by revealing a card with the dominant rank; that card's suit is now the *trump suit*. (This is the *weak declaration* mentioned above.) 
+
+Revealing two of the same card (both of the dominant rank) will cancel any prior weak declaration.
+
+*In the regular game, revealing a pair of jokers will declare no-trump rounds. This is not currently supported by this bot, but it may be implemented in the future.*
+
+After everyone has drawn 25 cards, the dealer integrates the remaining eight cards into his hand, and will choose eight cards from his hand to discard. These cards are known as the *kitty*, and will not be used in the upcoming round until the end. 
+
+Play then starts with the dealer, who may lead a trick of any number of cards, which the other players must match:
+
+* Doubles require other doubles in the same suit to be played (if no doubles, then singles in the same suit).
+* Tractors (consecutive doubles; see Appendix C) require first tractors, then doubles, then singles in the same suit.
+* Other plays are considered *shuǎi* (throws), and can *only* be played when the cards cannot be proven to not be the largest in their suit.
+* * If someone does have cards that can beat a single or double in this play (and note they have to be in the same suit), the player that led the trick *must* take back every other card and play the lowest in that category. (see Appendix D)
+* If a player is out of cards in a certain suit, the player can play a card from a different suit; unless the card is a trump, the card has the lowest possible priority.
+
+The person with the highest play takes the trick and (if they are not on the declarers' team) any *points* associated with it. Points are calculated for a trick by the number of `5`s, `10`s, and `K`s among the cards in the trick (`5` worth 5 points each, `10` and `K` 10 points each.)
+
+Only the opponents (aka not declarers) actually count their points. If the opponents win the last trick, the cards in the kitty are revealed, and the points underneath doubled and added to the total.
+
+### Result of each round
+
+If the opponents have:			Then the declarers are:		And their score increases by:
+
+* 0 points						Declarers					+3
+* 5-35 points					Declarers					+2
+* 40-75 points					Declarers					+1
+* 80-115 points				Opponents				0
+* 120-155 points				Opponents				+1
+* 160-195 points				Opponents				+2
+* at least 200 points			Opponents				+3
+
+## Appendix C: What is a tractor?
+
+Assume, for the sake of argument, that the trumps are `9C`.
+
+Tractors:
+`5H 5H 4H 4H`
+`6D 6D 5D 5D 4D 4D 3D 3D`
+`8C 8C 7C 7C 6C 6C`
+`10S 10S 8S 8S`
+`9C 9C 9D 9D AC AC`
+`J+ J+ J- J- 9C 9C`
+
+Not tractors:
+`5D 5D 4S 4S` (not the same suit)
+`6D 6D 4D 4D` (not consecutive)
+`9H 9H 8H 8H` (trumps + non-trumps)
+`9H 9H 9D 9D` (equal rank trumps)
+`9C 9C 8C 8C` (not consecutive)
+
+## Appendix D: Rules regarding shuǎi
+
+Again, assume, for the sake of argument, that the trumps are `9C`.
+
+Leader: `KD QD`
+
+`KD XX`: Leader must play only `QD`.
+`AD XX`: Leader must play only `QD`.
+`10C 3C`: Leader need not take back cards, but person playing this hand uses trumps to take the trick.
+
+Leader: `KD QD QD 10D 10D`
+
+`AD XX XX XX XX`: Leader must play only `KD`.
+`JD JD XX XX XX`: Leader must play only `10D 10D`.
+`8C 8C 5C 5C JC`: Leader need not take back cards, but person playing this hand uses trumps to take the trick. A person with higher trump doubles may take the trick. If equally ranked, the first person to play takes the trick.
--- shengji.py
+++ shengji.py
@@ -10,26 +10,40 @@ client = commands.Bot(command_prefix='$')
 
 # strings galore
 players = []
-winds = ["North (\u5317 b\u011bi)", "East (\u6771 d\014dng)", "South (\u5357 nán)", "West (\u897f x\012b)"]
-numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "keycap_ten", "regional_indicator_j", "regional_indicator_q", "regional_indicator_k", "regional_indicator_a"]
+winds = ["North (\u5317 b\u011bi)", "West (\u897f x\012b)", "South (\u5357 nán)", "East (\u6771 d\014dng)"]
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "keycap_ten", "regional_indicator_j", "regional_indicator_q", "regional_indicator_k", "regional_indicator_a", "trophy"]
 ranks = ["", "", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 suits = ["C", "D", "H", "S"]
-suits_long = ["clubs", "diamonds", "hearts", "spades"]
+suits_long = ["clubs", "diamonds", "hearts", "spades", "joker"]
 commandwords = ["intro", "join", "leave", "list", "start", "deal", "call", "kitty", "play", "score", "surrender", "reset"]
 
+#settings
+ruleset = 0 #bitmath... explained below
+# least significant - 1: scores for 5, 10, K are now mandatory to play
+# 2: chao di pi
+# 4: multiply kitty by number of cards used to win last score for opponents
+# 8: hook-back
+# most significant - 16: red 5 rule - no implementation yet
+
 #game data
-gameStarted = False #if game has started
+gameStarted = False #true if game has started
 score = [2, 2] #[NS, WE]
-dominantSuit = -1 #0C 1D 2H 3S
-dealer = -1 #0N 1E 2S 3W
-turn = -1 #0N 1E 2S 3W
+dominantSuit = -1 # “主牌” 0C 1D 2H 3S 4N
+currentSuit = -1 # 0C 1D 2H 3S 4T
+dealer = -1 # “庄” 0N 1W 2S 3E
+turn = -1 #0N 1W 2S 3E
 turncounter = 0 #0 1 2 3 looping from person with current hand
-points = 0 #non-dealer team
-cardlist = [] #stores card hierarchy
+shuai = False
+singles = []
+doubles = []
+points = 0 # “分” for non-dealer team only
+cardlist = [] #temporary storage unit for the card hierarchy for card sorting purposes in sendcardssorted; determining winners goes in a different category
 called = False #True if weakcalled
 strongcalled = False #True if strongcalled
 dealing = False #True if cards are being or have been dealt
+
 hands = [[],[],[],[]]
+plays = []
 reset = [False, False, False, False] #people who want to reset
 deleteque = []
 deleteque2 = []
@@ -38,7 +52,7 @@ kittycards = []
 #dominant suit in string form: suits[dominantSuit]
 #dominant rank: ranks[score[dealer%2]]
 
-def sendcardssorted(i):
+async def sendcardssorted(i):
     handembed = discord.Embed(title="Your hand (sorted)", description="", colour=discord.Colour.light_grey())
     playerhand = ["", ""]
     for j in range(54):
@@ -53,16 +67,63 @@ def sendcardssorted(i):
             else:
                 for k in range(hands[i].count(cardlist[j])):
                     playerhand[1] += ":{0}::{1}:\n".format(numbers[ranks.index(cardlist[j][:-1])], suits_long[suits.index(cardlist[j][-1])])
-#contingency for no trumps/other (rare)
+#contingency for no trumps/other (more near the end game)
     if playerhand[0] != "":
         handembed.add_field(name="Trumps",value=playerhand[0])
     if playerhand[1] != "":
         handembed.add_field(name="Others",value=playerhand[1])
-    client.send_message(players[i], "*TEMPORARY*", em=handembed)
+    await client.send_message(players[i], "*TEMPORARY*", em=handembed)
+
+def singledouble(cards):
+	for c in cards:
+		if cards.count(c) == 1:
+			singles.append(c)
+		elif not cardlist.index(c) in doubles:
+		 	doubles.append(c)
+	singles.sort(key = hierarchy(str)) #hopefully able to sort cards by order in hierarchy ._.
+	doubles.sort(key = hierarchy(str))
+	if len(singles) > 0:
+		return False #shuǎi
+	for i in range(len(doubles)-1):
+		if hierarchy(doubles[i]) != hierarchy(doubles[i+1]) - 1:	#Tractor handling
+			return False #shuǎi
+	return True #doubles or consecutive doubles
+
+def insuit(card, suit):
+	if suit == dominantSuit:
+		return hierarchy(card) > 15
+	else:
+		return card[1]==suits[suit]
+
+def hierarchy(card):
+	if card == "J+":
+		return 31
+	if card == "J-":
+		return 30
+	if card == ranks[score[dealer%2]] + suits[dominantSuit]: #dominant rank of dominant suit
+		return 29
+	if card[0] == ranks[score[dealer%2]]: #indistinguishable dominant ranks
+		return 28 + (1 if dominantSuit = 4) #wú zhǔ
+	if card[1] == suits[dominantSuit]:
+		return 14 + ranks.index(card[0]) - (1 if ranks.index(card[0]) > score[dealer%2])
+	if card[1] == suits[currentSuit]:
+		return ranks.index(card[0]) - (1 if ranks.index(card[0]) > score[dealer%2])
+	return 0
+	#0 (not current suit not trump, cannot form tractor), 2 3 4 5 6 7 8 9 10 11 12 13 (skip trump rank), then 16, 17, 18... 27 (again skip trump rank), 28, 29, 30 (J-), 31 (J+)
+
+def comparestructure(cards, cardcount, doublecount, tractorcount):
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_status(game=discord.Game(name="Tractor"))
+
+async def on_server_join(server):
+    dealerRole = create_role(server, name="Dealer", colour=discord.Colour.default())
+    everyone_perms = discord.PermissionOverwrite(read_messages=False)
+	my_perms = discord.PermissionOverwrite(read_messages=True)
+    dealerChannel = create_channel(server, "dealer-den", (server.default_role, everyone_perms), (dealerRole, my_perms))
 
 @client.command(pass_context=True)
 async def intro(ctx):
@@ -100,8 +161,10 @@ async def leave(ctx):
 
 @client.command(pass_context=True)
 async def list(ctx):
+    if len(players) == 0:
+        await ctx.bot.send_message(ctx.message.channel, 'No one to list. :shrug:')
+        return
     playerlist = discord.Embed(title="Players", description="", colour = discord.Colour.light_grey())
-        
     for i in range(len(players)):
         playerlist.add_field(name = winds[i], value = players[i])
     await ctx.bot.send_message(ctx.message.channel, embed=playerlist)
@@ -112,13 +175,10 @@ async def start(ctx):
         await ctx.bot.send_message(ctx.message.channel, ':x: Please say $reset to reset the game.')
         return
     if len(players) > 4:
-        await ctx.bot.send_message(ctx.message.channel, ':x: Too many playe-- wait. How did you even do that?')
+        await ctx.bot.send_message(ctx.message.channel, ':x: Too many playe-- wait. How did you even do that? I\'m `{0}.{1}%` confident that I had a safeguard against this scenario...'.format(randint(90,100), randint(0, 10)))
         return
     if len(players) != 4:
         await ctx.bot.send_message(ctx.message.channel, ':x: Not enough players! You need :{0}: more.'.format(numbers[4-len(players)]))
-        return
-    if score[0]==15 or score[1]==15:
-        await ctx.bot.send_message(ctx.message.channel, ':x: Please say $reset to reset the game.')
         return
     if gameStarted:
         await ctx.bot.send_message(ctx.message.channel, ':flower_playing_cards: Game has already started.')
@@ -128,30 +188,32 @@ async def start(ctx):
 
 @client.command(pass_context=True)
 async def deal(ctx):
+    global dealing
     if dealing:
         await ctx.bot.send_message(ctx.message.channel, ':x: We\'re already doing that!')
-    if not ctx.message.author in players:
-        await ctx.bot.send_message(ctx.message.channel, ':x: Who are you?')
-        return
     if score[0]==15 or score[1]==15:
         await ctx.bot.send_message(ctx.message.channel, ':x: Please say $reset to reset the game.')
         return
     if not gameStarted:
         await ctx.bot.send_message(ctx.message.channel, ':x: Game needs to start first.')
         return
+    if not ctx.message.author in players:
+        await ctx.bot.send_message(ctx.message.channel, ':x: Who are you?')
+        return
 #   game setup area
     dealing = True
     cardFile = open('cards.txt', 'r')
     cardlist.clear()
-    for i in range(108):
+    for i in range(54):
         cardlist.append(cardFile.readline())
+	cardlist *= 2
 #   success message
-    await ctx.bot.send_message(ctx.message.channel, ':{0}: Dealing cards. :{0}:'.format(numbers[score[dealer%2]]))
+    await ctx.bot.send_message(ctx.message.channel, ':{0}: Dealing cards. The dominant rank is :{0}:'.format(numbers[score[dealer%2]]))
 #   card dealer
     for i in range(25):
         for j in range(4):
             x = random.randint(0, len(cardlist))
-            hands[j].append(cardlist.pop(x))
+            hands[(dealer+j)%4].append(cardlist.pop(x))
             if len(deleteque)>=4:
                 await ctx.bot.delete_message(deleteque.pop(0))
             sendcardssorted(j)
@@ -164,11 +226,19 @@ async def deal(ctx):
             await ctx.bot.send_message(ctx.message.channel, '`{0}` seconds left.'.format((5-i)*5))
         else:
             break
+    if not dominantSuit == -1 and not strongcalled:
+        await ctx.bot.send_message(ctx.message.channel, 'You have `30` seconds to override this trump suit with a double.')
+        for i in range(6):
+            time.sleep(5)
+            if dominantSuit == -1:
+                await ctx.bot.send_message(ctx.message.channel, '`{0}` seconds left.'.format((5-i)*5))
+            else:
+                break
 #   contingency for no one calling a trump suit
     if dominantSuit == -1:
         x = random.randint(0, len(cardlist))
-        if cardlist[x][1] != '+' and cardlist[x][1] != '-':
-            dominantSuit = suits.index(x[1])
+        if cardlist[x][-1] != '+' and cardlist[x][-1] != '-':
+            dominantSuit = suits.index(x[-1])
             await ctx.bot.send_message(ctx.message.channel, 'Auto-call:')
         else:
             await ctx.bot.send_message(ctx.message.channel, 'No trump called. Type $deal to redeal.')
@@ -177,7 +247,7 @@ async def deal(ctx):
             dealing = False
             return
     await ctx.bot.send_message(ctx.message.channel, 'The trump suit is :{0}: `{0}`.'.format(suits_long[dominantSuit]))
-    turn = dealer
+    await ctx.bot.add_roles(players[dealer], dealerRole)
     for i in range(8):
         hands[dealer].append(cardlist.pop(0))
 #   kitty reorganization - see kitty command later
@@ -214,8 +284,8 @@ async def call(ctx, suit, strong: typing.Optional[int] = 0):
     if strongcalled:
         await ctx.bot.send_message(ctx.message.channel, ':x: Call is no longer possible!')
         return
-
     if strong != 0:
+        
         if hands[players.index(ctx.message.author)].count("{0}{1}".format(ranks[score[dealer%2]],suit)) != 2:
             await ctx.bot.send_message(ctx.message.channel, ':x: No {0}{1} pair detected'.format(ranks[score[dealer%2]],suit))
             return
@@ -240,6 +310,7 @@ async def call(ctx, suit, strong: typing.Optional[int] = 0):
 
 @client.command(pass_context=True)
 async def kitty(ctx, *cards):
+	
     if not gameStarted:
         await ctx.bot.send_message(ctx.message.channel, ':x: No game in progress. Command unusable.')
         return
@@ -255,17 +326,106 @@ async def kitty(ctx, *cards):
     if not dealing:
         await ctx.bot.send_message(ctx.message.channel, ':x: Where are your cards?')
         return
+	if ctx.message.channel != dealerChannel:
+		await ctx.bot.delete_message(ctx.message)
+		await ctx.bot.send_message(ctx.message.channel, ':x: This is the wrong place for that. Go to {0}.'.format(dealerChannel))
+        return
     if len(cards) != 8:
         await ctx.bot.send_message(ctx.message.channel, ':x: There must be exactly eight cards in the kitty.')
         return
     for c in cards:
-        if cards.count(c) != hands[dealer].count(c):
+        if cards.count(c) > hands[dealer].count(c):
             await ctx.bot.send_message(ctx.message.channel, ':x: You don\'t have enough `{0}` to do this.'.format(c))
             return
     kittycards = list(cards)
     for c in kittycards:
         hands[dealer].remove(c)
-    await ctx.bot.send_message(ctx.message.channel, ':cat: Kitty set. You start the game (type `$play [card]`).')
+    await ctx.bot.send_message(ctx.message.channel, ':o: Kitty set. You start the game (type `$play [cards]`).')
+    global turn
+    turn = dealer
+
+@client.command(pass_context=True)
+async def play(ctx, *cards):
+    if not gameStarted:
+        await ctx.bot.send_message(ctx.message.channel, ':x: No game in progress. Command unusable.')
+        return
+    if score[0]==15 or score[1]==15:
+        await ctx.bot.send_message(ctx.message.channel, ':x: Please say $reset to reset the game.')
+        return
+    if not ctx.message.author in players:
+        await ctx.bot.send_message(ctx.message.channel, ':x: Who are you?')
+        return
+    if turn == -1:
+        await ctx.bot.send_message(ctx.message.channel, ':x: Please wait for the kitty to be set.')
+        return
+    if players[(turn+turncounter)%4] != ctx.message.author:
+        await ctx.bot.send_message(ctx.message.channel, ':x: It is not your turn!')
+        await ctx.bot.delete_message(ctx.message)
+        return
+    for c in cards:
+    	if not c in cardlist:
+			await ctx.bot.send_message(ctx.message.channel, ':x: That\'s not a card.'.format(c))
+            return
+        if cards.count(c) > hands[dealer].count(c):
+            await ctx.bot.delete_message(ctx.message)
+            await ctx.bot.send_message(ctx.message.channel, ':x: You don\'t have enough `{0}` to do this.'.format(c))
+            return
+    #card setsuit - initial
+    EmbedPlay = discord.Embed(title="Played cards", description="", colour=discord.Colour.light_grey)
+    if turncounter==0:
+    	#determine if all cards in one suit
+    	currentSuit = dominantSuit if hierarchy(cards[0]) > 15 else cards[0][1]
+    	for c in cards:
+    		if hierarchy(c) == 0:
+				await ctx.bot.delete_message(ctx.message)
+            	await ctx.bot.send_message(ctx.message.channel, ':x: Cards must be of the same suit.'.format(c))
+            	return
+			else if currentSuit != dominantSuit and hierarchy(c) > 15:
+				await ctx.bot.delete_message(ctx.message)
+            	await ctx.bot.send_message(ctx.message.channel, ':x: Cards must be of the same suit.'.format(c))
+            	return
+		#determine if singles, doubles, or tractor
+		shuai = False if len(cards)==1 else not singledouble(cards)
+		#if none of the above, make sure shuǎi is permissible
+		#if not permissible, play one card/pair of cards depending on what is matched
+		if shuai == True:
+			for i in cardlist:
+				if hierarchy(i) > hierarchy(singles[0]) and (hierarchy(singles[0]) > 15 or hierarchy(i) < 14): #same suit, and i > c
+					if i in hands[1] or i in hands[3]:
+						EmbedPlay.add_field(name="Shuǎi denied!", value="Another person has a card higher than `{0}`. You are now forced to play this card.".format(singles[0]))
+						plays.append([singles[0]])
+						break
+				if hierarchy(i) > hierarchy(doubles[0]) and (hierarchy(doubles[0]) > 15 or hierarchy(i) < 14):
+					if hands[1].count(i)==2 or hands[3].count(i)==2:
+						EmbedPlay.add_field(name="Shuǎi denied!", value="Another person has a pair of cards higher than `{0}`. You are now forced to play this pair.".format(doubles[0]))
+						plays.append([doubles[0],doubles[0]])
+						break
+		#play the hand
+		if len(plays)==0:
+			plays.append(cards)
+    else:
+    	#make sure they match card number else disaster happens
+    	if len(cards) != len(plays[0])
+		#look at cards in suit in player hand, and count cards, doubles, and tractors... (can you do that when the game starts to reduce runtime?)
+		#if cards in suit >= total card count in initial hand, must match with cards in suit; else play all cards of that suit, then start playing cards of their choice
+		#if doubles in suit >= total doubles in initial hand, they must play that many doubles; else doubles must equal no. doubles in hand
+		#if exists a tractor of at least same size as inital tractor, must play a tractor
+		#play the hand
+	if turncounter == 3:
+		#if structure is matched by one hand that is all trumps, goes there
+		#else determine by highest card in hierarchy
+		#increment point counts if opponents win
+		#change who gets the turn-
+    #turn shift
+	if len[hands[0]] == 0 and len[hands[1]] == 0 and len[hands[2]] == 0 and len[hands[3]] == 0:
+		#if dealer score 14 and dealer's team took last trick and last trick high card was an ace then make it 15 and it's game over
+		#change dealer by point count
+		#increase score by point count
+		#if anything is bigger than 14 reduce to 14
+		dealing = False
+    turncounter = (turncounter + 1)%4
+    await ctx.bot.send_message(ctx.message.channel, 'It is now {0.mention}\'s turn.'.format(players[(turncounter+turn)%4]))
+
 
 @client.command(pass_context=True)
 async def reset(ctx):
@@ -279,7 +439,7 @@ async def reset(ctx):
         reset[players.index(ctx.message.author)] = True
         await ctx.bot.send_message(ctx.message.channel, ':ballot_box: Reset vote recorded. :{0}: more required.'.format(numbers[3-reset.count(True)]))
         if reset.count(True)==3:
-        #initiate reset
+        #resetting here
             await ctx.bot.send_message(ctx.message.channel, ':gear: Reset complete.')
     else:
         reset[players.index(ctx.message.author)] = False
@@ -299,9 +459,7 @@ async def on_message(message):
         await client.process_commands(message)
     else:
         if "*TEMPORARY*" in message.content:
-            deleteque.append(message.id)
-        if "__TEMPORARY__" in message.content:
-            deleteque2.append(message.id)
+            deleteque.append(message)
 
 with open('test.json') as f:
     token = json.load(f)
