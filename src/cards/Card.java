34a4465781ff29e6a406ed96262a2ed8746b2096
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-17 13:50:42 -0500

    Stuff

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -1,11 +1,15 @@
 package cards;
 
+import java.util.Comparator;
+
 import game.GameData;
 import game.Round;
 
 public class Card {
 	private final int rank; //2-13, 14 = A, 15 = J
 	private final int suit; //1C 2D 3H 4S
+	private static final String[] numbers = {"zero","one","two","three","four","five","six","seven","eight","nine","keycap_ten","regional_indicator_j","regional_indicator_q","regional_indicator_k","regional_indicator_a","small_red_triangle_down","small_red_triangle"};
+	private static final String[] suits = {"joker","clubs","diamonds","hearts","spades"};
 	
 	public Card() {
 		rank = 0;
@@ -15,24 +19,43 @@ public class Card {
 		rank = r;
 		suit = s;
 	}
-	public int getHierarchy(Round round) {
-		if (rank == 16) return 31;
-		if (rank == 15) return 30;
+	
+	static class comparator implements Comparator<Card> {
+		@Override
+		public int compare(Card o1, Card o2) {
+			if (o1.getHierarchy()==o2.getHierarchy()) {
+				if (o1.suit==o2.suit) {
+					if (o1.rank==o2.rank) return 0;
+					else if (o1.rank<o2.rank) return -1;
+					else return 1;
+				}
+				else if (o1.suit < o2.suit) return -1;
+				else return 1;
+			}
+			return (o1.getHierarchy()-o2.getHierarchy())/Math.abs(o1.getHierarchy()-o2.getHierarchy());
+		}
+		
+	}
+	
+	public int getHierarchy() {
+		if (rank >= 15) return rank + 15;
 		if (rank == GameData.getDominantRank()) return 29;
 		if (suit == GameData.getDominantSuit()) return 14+rank-(rank>GameData.getDominantRank()?1:0); //17-28
-		if (suit == round.getRoundSuit()) return rank-(rank>GameData.getDominantRank()?1:0); //2-13
+		if (suit == Round.getRoundSuit()) return rank-(rank>GameData.getDominantRank()?1:0); //2-13
 		return 0;
 	}
 	public int getRank() {
 		return rank;
 	}
 	public String getSuit() {
-		String[] suits = {null,"clubs","diamonds","hearts","spades"};
 		return suits[suit];
 	}
 	public String getSuitShort() {
 		if (getSuit()==null) return "N";
 		return getSuit().substring(0,1).toUpperCase();
+	}
+	public String toString() {
+		return ":" + numbers[rank] + "::" + suits[suit] + ":";
 	}
 	
 }
--- src/game/Round.java
+++ src/game/Round.java
@@ -5,14 +5,14 @@ import java.util.List;
 import cards.Card;
 
 public class Round {
-	private int RoundSuit;
+	private static int RoundSuit;
 	private List<List<Card> > plays;
 	
 	public void uploadPlay(List<Card> cards) {
 		plays.add(cards);
 	}
 	
-	public int getRoundSuit() {
+	public static int getRoundSuit() {
 		// TODO Auto-generated method stub
 		return RoundSuit;
 	}
