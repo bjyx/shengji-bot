a1981401edc2c52b195ff5478ebf0a67c8ac509c
[REDACTED] [REDACTED] <wes4zhang@gmail.com>
2019-02-19 03:50:05 -0800

    Update PlayerList.java

--- src/game/PlayerList.java
+++ src/game/PlayerList.java
@@ -2,7 +2,6 @@ package game;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.MessageEmbed;
